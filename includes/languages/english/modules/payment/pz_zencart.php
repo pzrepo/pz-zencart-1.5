f4e039cb30294b04a6a08b97785ccd1adf609c03
Jinesh <jinesh.d@paymentz.com>
2018-01-12 13:21:07 +0530

    pz-zencart-v1.5 plugin

---
+++ includes/languages/english/modules/payment/pz_zencart.php
@@ -0,0 +1,9 @@
+<?php
+	define('MODULE_PAYMENT_PZ_ZENCART_TEXT_TITLE', 'Pz_Zencart');
+	define('MODULE_PAYMENT_PZ_ZENCART_TEXT_DESCRIPTION', 'Please make your check or money order payable to:<br />' . MODULE_PAYMENT_PZ_ZENCART_MERCHANT_ID . '<br />');
+	define('MODULE_PAYMENT_PZ_ZENCART_TEXT_EMAIL_FOOTER', "Please make your check or money order payable to:" . "\n\n" . MODULE_PAYMENT_PZ_ZENCART_MERCHANT_ID . "\n");\define('MODULE_PAYMENT_PZ_ZENCART_TEXT_DESCRIPTION', ' Enable Pz ZenCart Online Payments');
+	define('MODULE_PAYMENT_PZ_ZENCART_TEXT_ERROR', 'Credit Card Error!');
+	define('MODULE_PAYMENT_PZ_ZENCART_TEXT_ERROR_MESSAGE', 'There has been an error processing you credit card at Pz ZenCart, please try again.');
+	define('MODULE_PAYMENT_PZ_ZENCART_ALERT_ERROR_MESSAGE', 'Security Error. Illegal access detected, please try again.');
+
+?>
